351d22d8eb186f1b63e4a1c6b2d172341a7c746e
Michael Driggs <azpilot@gmail.com>
2024-06-11 16:37:23 -0600

    eroor fixed

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='flex w-full justify-between items-start px-[5%] py-[1%]'>
       <div className='flex w-3/5 justify-start items-start flex-col pt-32'>
         <h1 className='text-8xl font-extrabold pb-8 text-[#4D1427]'>Elevate Your Online Presence with Our Expertise.</h1>
-        <h2 className='text-4xl pb-4'>Let's Craft Your Digital Success Story Together!</h2>
+        <h2 className='text-4xl pb-4'>Let&apos;s Craft Your Digital Success Story Together!</h2>
         <p>Welcome to Luna Design – Your Premier Destination for Stunning Web Design and Development Solutions!</p>
         <div className='flex gap-4 mt-4'>
             <Button className='bg-[#F44C41] text-white w-fit h-[32px] p-8 text-2xl'>Pricing and Plans</Button>
